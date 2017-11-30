bbaef1d09e540dfb5a7224d89a145ecb545bba24
Zoe <jinsuyi@gmail.com>
2017-11-30 12:43:04 +0100

    remove old codes

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 import unittest
 
-from main import merge_intervals, intervals_sub, remove_overlap
+from main import merge_intervals, intervals_sub
 
 
 class TestMerge(unittest.TestCase):
@@ -11,8 +11,7 @@ class TestMerge(unittest.TestCase):
         merged_includes = merge_intervals(includes)
         merged_excludes = merge_intervals(excludes)
         after_sub = intervals_sub(merged_includes, merged_excludes)
-        result = remove_overlap(after_sub)
-        self.assertEqual(result, [(10, 19), (31, 100)])
+        self.assertEqual(after_sub, [(10, 19), (31, 100)])
 
     def test_case_2(self):
         includes = [(50, 5000), (10, 100)]
@@ -20,8 +19,7 @@ class TestMerge(unittest.TestCase):
         merged_includes = merge_intervals(includes)
         merged_excludes = merge_intervals(excludes)
         after_sub = intervals_sub(merged_includes, merged_excludes)
-        result = remove_overlap(after_sub)
-        self.assertEqual(result, [(10, 5000)])
+        self.assertEqual(after_sub, [(10, 5000)])
 
     def test_case_3(self):
         includes = [(10, 100), (200, 300)]
@@ -29,8 +27,7 @@ class TestMerge(unittest.TestCase):
         merged_includes = merge_intervals(includes)
         merged_excludes = merge_intervals(excludes)
         after_sub = intervals_sub(merged_includes, merged_excludes)
-        result = remove_overlap(after_sub)
-        self.assertEqual(result, [(10, 94), (206, 300)])
+        self.assertEqual(after_sub, [(10, 94), (206, 300)])
 
     def test_case_4(self):
         includes = [(10, 100), (200, 300), (400, 500)]
@@ -38,8 +35,7 @@ class TestMerge(unittest.TestCase):
         merged_includes = merge_intervals(includes)
         merged_excludes = merge_intervals(excludes)
         after_sub = intervals_sub(merged_includes, merged_excludes)
-        result = remove_overlap(after_sub)
-        self.assertEqual(result, [(10, 94), (206, 300), (400, 409), (421, 500)])
+        self.assertEqual(after_sub, [(10, 94), (206, 300), (400, 409), (421, 500)])
 
     def test_case_5(self):
         includes = [(90, 100), (50, 60), (20, 190), (400, 550), (700, 800), (230, 410)]
@@ -47,8 +43,7 @@ class TestMerge(unittest.TestCase):
         merged_includes = merge_intervals(includes)
         merged_excludes = merge_intervals(excludes)
         after_sub = intervals_sub(merged_includes, merged_excludes)
-        result = remove_overlap(after_sub)
-        self.assertEqual(result, [(20, 149), (270, 499), (751, 800)])
+        self.assertEqual(after_sub, [(20, 149), (270, 499), (751, 800)])
         
     def test_case_6(self):
         includes = [(10, 100), (200, 300), (400, 500), (600,800)]
@@ -56,8 +51,7 @@ class TestMerge(unittest.TestCase):
         merged_includes = merge_intervals(includes)
         merged_excludes = merge_intervals(excludes)
         after_sub = intervals_sub(merged_includes, merged_excludes)
-        result = remove_overlap(after_sub)
-        self.assertEqual(result, [(10, 94), (206, 264), (296, 300)])
+        self.assertEqual(after_sub, [(10, 94), (206, 264), (296, 300)])
     
     def test_case_7(self):
         includes = [(1,200), (400,500), (600,700)]
@@ -65,8 +59,7 @@ class TestMerge(unittest.TestCase):
         merged_includes = merge_intervals(includes)
         merged_excludes = merge_intervals(excludes)
         after_sub = intervals_sub(merged_includes, merged_excludes)
-        result = remove_overlap(after_sub)
-        self.assertEqual(result, [(1, 9), (21, 29), (41, 49), (61, 200)])
+        self.assertEqual(after_sub, [(1, 9), (21, 29), (41, 49), (61, 200)])
         
     def test_case_8(self):
         includes = [(1,200), (300,400)]
@@ -74,8 +67,7 @@ class TestMerge(unittest.TestCase):
         merged_includes = merge_intervals(includes)
         merged_excludes = merge_intervals(excludes)
         after_sub = intervals_sub(merged_includes, merged_excludes)
-        result = remove_overlap(after_sub)
-        self.assertEqual(result, [(6, 9), (21, 29), (41, 49), (61, 200), (300, 400)])
+        self.assertEqual(after_sub, [(6, 9), (21, 29), (41, 49), (61, 200), (300, 400)])
         
     def test_case_9(self):
         includes = [(1,200), (300,400), (500,600)]
@@ -83,8 +75,7 @@ class TestMerge(unittest.TestCase):
         merged_includes = merge_intervals(includes)
         merged_excludes = merge_intervals(excludes)
         after_sub = intervals_sub(merged_includes, merged_excludes)
-        result = remove_overlap(after_sub)
-        self.assertEqual(result, [(6, 9), (21, 29), (500, 600)])
+        self.assertEqual(after_sub, [(6, 9), (21, 29), (500, 600)])
         
     def test_case_10(self):
         includes = [(1,200), (300,400), (500,600)]
@@ -92,8 +83,7 @@ class TestMerge(unittest.TestCase):
         merged_includes = merge_intervals(includes)
         merged_excludes = merge_intervals(excludes)
         after_sub = intervals_sub(merged_includes, merged_excludes)
-        result = remove_overlap(after_sub)
-        self.assertEqual(result, [(1, 200), (300, 319), (331, 339), (351, 400), (500, 600)])
+        self.assertEqual(after_sub, [(1, 200), (300, 319), (331, 339), (351, 400), (500, 600)])
         
     def test_case_11(self):
         includes = [(1,200), (300,400), (500,600), (700,800), (900,1000)]
@@ -101,8 +91,7 @@ class TestMerge(unittest.TestCase):
         merged_includes = merge_intervals(includes)
         merged_excludes = merge_intervals(excludes)
         after_sub = intervals_sub(merged_includes, merged_excludes)
-        result = remove_overlap(after_sub)
-        self.assertEqual(result, [(1, 119), (331, 339), (351, 400), (500, 600), (700, 800), (900, 919), (931, 1000)])
+        self.assertEqual(after_sub, [(1, 119), (331, 339), (351, 400), (500, 600), (700, 800), (900, 919), (931, 1000)])
 
     
 
